331ee2dd9e17e7f362d4c689a5d76462e1c736c2
vitale71 <lucasvitale71@gmail.com>
2025-08-28 17:45:31 -0300

    .

---
+++ Guia3.3/Ejercicio1/Models/Actividad.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio1.Models
+{
+    public abstract class Actividad
+    {
+        public int Periodo { get; set; }
+        public string Descripcion { get; set; }
+        public List<Parcela> LotesAsignados = new List<Parcela>();
+        public Actividad(int periodo, string descripcion, Parcela p) 
+        { 
+        
+        }
+
+        abstract public double CalcularRendimiento();
+
+        abstract public double CalcularProduccion();
+
+    }
+}
